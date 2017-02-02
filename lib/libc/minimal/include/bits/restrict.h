69f680148394e567736545a488d0cca851d4af80
Andrew Boie <andrew.p.boie@intel.com>
2017-02-02 12:01:59 -0800

    libc: minimal: rename private macro
    As it turns out Xtensa SDK headers also define _Restrict, causing
    havoc. As this was intended to be a private macro, rename it to something
    less likely to cause a collision.
    
    Change-Id: I0a7501a1af8cf87efb096872a91a7b44bd2bbdca
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- lib/libc/minimal/include/bits/restrict.h
+++ lib/libc/minimal/include/bits/restrict.h
@@ -6,20 +6,20 @@
 
 /**
  * @file
- * @brief _Restrict definition
+ * @brief _MLIBC_RESTRICT definition
  *
- * The macro "_Restrict" is intended to be private to the minimal libc library.
- * It evaluates to the "restrict" keyword when a C99 compiler is used, and
- * to "__restrict__" when a C++ compiler is used.
+ * The macro "_MLIBC_RESTRICT" is intended to be private to the minimal libc
+ * library.  It evaluates to the "restrict" keyword when a C99 compiler is
+ * used, and to "__restrict__" when a C++ compiler is used.
  */
 
-#if !defined(_Restrict_defined)
-#define _Restrict_defined
+#if !defined(_MLIBC_RESTRICT_defined)
+#define _MLIBC_RESTRICT_defined
 
-#if defined(__cplusplus) || defined(__xtensa__)
-	#define _Restrict __restrict__
+#if defined(__cplusplus)
+	#define _MLIBC_RESTRICT __restrict__
 #else
-	#define _Restrict restrict
+	#define _MLIBC_RESTRICT restrict
 #endif
 
 #endif
